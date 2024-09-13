6cfa97d5367e1d910d931a788e6ac4b4053a6718
Zeke-MA <gregorios.tzikas@gmail.com>
2024-09-12 22:58:19 -0400

    Clock Instance created

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         pygame.Surface.fill(screen,color=000000)
         pygame.display.flip()
         
